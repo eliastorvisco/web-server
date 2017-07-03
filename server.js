eb0958028c91084467746b544860fb7fcd77701e
Elias Torvisco <eliastorvisco.macia@gmail.com>
2017-07-03 17:39:42 +0200

    Initialize repo

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+const PORT = 3000;
+
+var middleware = {
+    requireAuthentication: function (request, response, next) {
+        console.log('private route hit!');
+        next();
+    },
+    logger: function (request, response, next) { //allows to see the requests
+        console.log('Request: ' + request.method + ' ' + request.originalUrl + ' [' + new Date().toString() + ']');
+        next();
+    }
+};
+
+//app.use(middleware.requireAuthentication); //It is important to locate it above app.get ... in the code.
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (request, response) {
+    response.send('About us');
+});
+
+app.use(express.static(__dirname + '/public')); //expose a folder. It needs the entire address.
+app.listen(PORT, function() {
+    console.log('Express server started at port ' + PORT + '!');
+});
